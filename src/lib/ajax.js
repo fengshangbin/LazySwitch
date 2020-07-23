2641fd7e6aa482a5a4cf31076d6a688e28d14aaf
shangbin <fengshangbin@126.com>
2020-07-23 12:56:17 +0800

    update

--- src/lib/ajax.js
+++ src/lib/ajax.js
@@ -1,59 +1,16 @@
 export default function ajax() {
   var ajaxData = {
-    type: arguments[0].type || 'GET',
     url: arguments[0].url || '',
-    async: arguments[0].async || 'true',
-    data: arguments[0].data || null,
-    dataType: arguments[0].dataType || 'text',
-    header: arguments[0].header || null,
-    contentType: arguments[0].contentType || 'application/x-www-form-urlencoded',
-    beforeSend: arguments[0].beforeSend || function() {},
-    success: arguments[0].success || function() {},
-    error: arguments[0].error || function() {}
+    success: arguments[0].success || function () { },
+    error: arguments[0].error || function () { }
   };
-  ajaxData.beforeSend();
-  var xhr = createxmlHttpRequest();
-  //xhr.responseType = ajaxData.dataType;
-  //ajaxData.data = convertData(ajaxData.data);
-  if (ajaxData.type.toLowerCase() == 'get' && ajaxData.data != null && ajaxData.data != '') {
-    ajaxData.url += ajaxData.url.indexOf('?') > 0 ? '&' : '?';
-    ajaxData.url += ajaxData.data;
-  }
-  xhr.open(ajaxData.type, ajaxData.url, ajaxData.async);
-  xhr.setRequestHeader('Content-Type', ajaxData.contentType);
-  if (ajaxData.header != null) {
-    for (var key in ajaxData.header) {
-      xhr.setRequestHeader(key, ajaxData.header[key]);
-    }
-  }
-  xhr.send(ajaxData.type.toLowerCase() == 'post' && ajaxData.data != null ? ajaxData.data : null);
-  xhr.onreadystatechange = function() {
-    if (xhr.readyState == 4) {
-      if (xhr.status == 200) {
-        ajaxData.success(xhr.responseText);
-      } else {
-        ajaxData.error(xhr.status);
-      }
-    }
-  };
+  var request = new XMLHttpRequest();
+  request.addEventListener('load', function () {
+    ajaxData.success(this.responseText);
+  });
+  request.addEventListener('error', function () {
+    ajaxData.error('error at request ' + ajaxData.url);
+  });
+  request.open('GET', ajaxData.url);
+  request.send();
 }
-
-function createxmlHttpRequest() {
-  /* if (window.ActiveXObject) {
-    return new ActiveXObject('Microsoft.XMLHTTP');
-  } else if (window.XMLHttpRequest) { */
-  return new XMLHttpRequest();
-  //}
-}
-
-/*function convertData(data){ 
-  if( typeof data === 'object' ){ 
-	let str = []
-	for (let key in data) {
-	  str.push(key + '=' + data[key])
-	}
-	return str.join('&');
-  }else{ 
-    return data; 
-  } 
-}*/
